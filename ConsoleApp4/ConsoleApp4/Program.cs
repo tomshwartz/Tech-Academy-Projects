ffb1745b3df245651a10cc11b11d158831357c36
Thomas Shwartz <tomshwartz@gmail.com>
2018-07-30 20:26:36 -0700

    Added boolean loops drill

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        //Boolean while statement
+        Console.WriteLine("How many hours did you study today?");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        while (num<8)
+        {
+            Console.WriteLine("Study More");
+            Console.WriteLine("How many hours have you studied now?");
+            num = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("You have studied enough");
+
+        //Boolean do while statement
+        Console.WriteLine("What is the tenth's place of PI?");
+        int guess = Convert.ToInt32(Console.ReadLine());
+        do
+        {
+            if (guess == 1)
+            {
+                Console.WriteLine("You're right.");
+                Console.ReadLine();
+            } else
+            {
+                Console.WriteLine("You're wrong try again.");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        while (guess != 1);
+
+    }
+}
+
