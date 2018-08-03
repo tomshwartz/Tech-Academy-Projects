a1f20373648939b14982fc3c74d2c1a76e856550
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-03 13:42:57 -0700

    Drill

---
+++ ClassMethods1/ClassMethods1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods1
+{
+    class Class1
+    {
+        public static void Walk(int num, out int num1)
+        {
+            num1 = num / 2;
+        }
+        public static void Walk(string num)
+        {
+
+        }
+    }
+}
---
+++ ClassMethods1/ClassMethods1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine("Enter a number");
+            int num1 = 0;
+            int num = Convert.ToInt16(Console.ReadLine());
+            Class1.Walk(num, out num1);
+            Console.WriteLine(num1);
+            Console.ReadLine();
+        }
+    }
+}
