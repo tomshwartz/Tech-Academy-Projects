a07752be9c5aeee6dcbe031288c7f8c3cf4f203e
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-03 13:09:44 -0700

    Added Drill

---
+++ ConsoleApp5/ConsoleApp5/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Math
+    {
+        public static void Add(int num1, int num2)
+        {
+            num1 = num1 * 2;
+            Console.WriteLine(num2);
+            return;
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Math.Add(10, 2);
+            Math.Add(num1: 2, num2: 4);
+            Console.ReadLine();
+        }
+    }
+}
