f379928a637708b172ea1c3071a699c6c87f7140
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-03 11:37:34 -0700

    Added Class/Methods drill 2

---
+++ ClassAndsMethods2/ClassAndsMethods2/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndsMethods2
+{
+    class Math
+    {
+        public static int Add(int num)
+        {
+            num = num + 5;
+            return num;
+        }
+
+        public static decimal Add(decimal num)
+        {
+            num = num + .2m;
+            return num;
+        }
+
+        public static int Add(string num)
+        {
+            int num1 = Convert.ToInt16(num);
+            num1 = num1 - 5;
+            return num1;
+        }
+    }
+}
---
+++ ClassAndsMethods2/ClassAndsMethods2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndsMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Math.Add(12);
+            Console.WriteLine(num);
+            decimal num1 = Math.Add(.5m);
+            Console.WriteLine(num1);
+            int num2 = Math.Add("10");
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
