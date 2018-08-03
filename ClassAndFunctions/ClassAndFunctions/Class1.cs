2a40f5b9f3f0e55b01d06d65aeb2e8624fe143c7
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-03 10:48:55 -0700

    Added Class/Function drill

---
+++ ClassAndFunctions/ClassAndFunctions/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndFunctions
+{
+    class Class1
+
+    {
+        public static int Add(int num1)
+        {
+            Console.WriteLine("How much do you want to add?");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            num1 = num1 + num2;
+            return num1;
+        }
+
+        public static int Multiply(int num1)
+        {
+            Console.WriteLine("How much do you want to multiply by?");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            num1 = num1 * num2;
+            return num1;
+        }
+
+        public static int Minus(int num1)
+        {
+            Console.WriteLine("How much do you want to subtract?");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            num1 = num1 - num2;
+            return num1;
+        }
+
+    }
+}
---
+++ ClassAndFunctions/ClassAndFunctions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Do you want to Multiply, Add, or Subtract?");
+            string choice = Console.ReadLine();
+            if (choice == "Multiply")
+            {
+                num1 = Class1.Multiply(num1);
+            } else if (choice == "Subtract")
+            {
+                num1 = Class1.Minus(num1);
+            } else if (choice == "Add")
+            {
+                num1 = Class1.Add(num1);
+            } else
+            {
+                Console.WriteLine("Your did not enter Multiply, Subract, or Add");
+            }
+
+
+
+
+
+
+
+
+
+            Console.WriteLine(num1);
+            Console.ReadLine();
+        }
+    }
+}
