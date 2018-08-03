7e916478549160b0ecd05df783587c8158bd18e7
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-03 12:55:45 -0700

    ClassAndMethods Drill

---
+++ ClassAndMethods3/ClassAndMethods3/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods3
+{
+    class Math
+    {
+        public static int Add(int num1, string num2 = "")
+        {
+            int num;
+            if (num2 == "")
+            {
+                num = 0;
+            } else
+            {
+                num = Convert.ToInt16(num2);
+            }
+            num = num1 + num;
+            return num;
+        }
+    }
+}
---
+++ ClassAndMethods3/ClassAndMethods3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number");
+            Int16 num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            string num2 = Console.ReadLine();
+
+
+            
+            Math math = new Math();
+            int num = Math.Add(num1, num2);
+            Console.WriteLine(num);
+            Console.ReadLine();
+        }
+    }
+}
