d7bb50a99f30b4ec79dff4a8e6bc20f240e21e76
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-08 15:23:27 -0700

    Added Struct Drill

---
+++ Struckts/Struckts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struckts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = .5m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
