6a38854fda0d1714ea67dee3370ad4adb44f3054
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-08 12:06:06 -0700

    Added Overload drill

---
+++ OverLoad/OverLoad/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoad
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public static bool operator== (Employee employee, Employee employee1)
+        {
+            bool check = (employee.Id == employee1.Id);
+            return check;
+        }
+        public static bool operator!= (Employee employee, Employee employee1)
+        {
+            bool check = (employee.Id == employee1.Id);
+            return check;
+        }
+    }
+}
---
+++ OverLoad/OverLoad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Check;
+            Employee employee = new Employee();
+            employee.Id = 1;
+            Employee employee1 = new Employee();
+            employee1.Id = 2;
+            
+            Console.WriteLine(employee == employee1);
+            Console.ReadLine();
+        }
+    }
+}
