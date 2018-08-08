33e90d9f890ae91c0cfdbba33c8e805d8289f0f2
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-08 10:56:22 -0700

    Added Page 125 Drill

---
+++ Interface/Interface/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Employee : Person, IQuittable 
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Program Quit");
+        }
+    }
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quittable = new Employee();
+            quittable.Quit();
+            Console.ReadLine();
+        }
+    }
+}
