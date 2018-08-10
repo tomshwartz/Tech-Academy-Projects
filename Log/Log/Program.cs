b93167f4d57e59f87257a6721d1a3686fc4494f7
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-10 14:33:11 -0700

    Log Drill

---
+++ Log/Log/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            string num = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\User\Desktop\log.txt", true))
+            {
+                file.WriteLine(num);
+            }
+            string text = File.ReadAllText(@"C:\Users\User\Desktop\log.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
