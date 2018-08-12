36952e5fd5e5412ef1ee1a71826bc3df1824a779
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-12 11:20:08 -0700

    Datetime Drill

---
+++ Datetime/Datetime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            DateTime dateTime = new DateTime();
+            dateTime = DateTime.Now;
+            Console.WriteLine("Enter a number");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(dateTime.AddHours(hour));
+            Console.ReadLine();
+
+        }
+    }
+}
