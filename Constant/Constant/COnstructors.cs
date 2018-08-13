c27932f5558df4020bc2d4002439edb63e41f971
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-13 13:11:11 -0700

    Constant Drill Added

---
+++ Constant/Constant/COnstructors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    public class COnstructors
+    {
+        public COnstructors(int Prop) : this(Prop,"Bill")
+        {
+
+        }
+        public COnstructors(int Prop, string name)
+        {
+            Prop1 = Prop;
+            Name = name;
+        }
+        public int Prop1 { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Constant/Constant/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int five = 5;
+            var name = "tom";
+
+        }
+    }
+}
