8e3986afa3575343cfa825e613068ac8cfd3f335
Thomas Shwartz <tomshwartz@gmail.com>
2018-08-14 09:55:30 -0700

    Error Execption Drill

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your age.");
+            bool valid = false;
+            int num = 0;
+            try
+            {
+                valid = int.TryParse(Console.ReadLine(), out num);
+                if (num > 0)
+                {
+                    Console.WriteLine(num);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number greater than 0.");
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error you entered a number less than 1.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
